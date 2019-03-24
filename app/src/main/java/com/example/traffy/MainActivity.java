4879ffafe687901aa0c748b658141daa0214564a
clintq18 <clinton.quashe@bluecrest.edu.gh>
2019-03-24 11:32:11 +0000

    Final commit

--- app/src/main/java/com/example/traffy/MainActivity.java
+++ app/src/main/java/com/example/traffy/MainActivity.java
@@ -2,22 +2,44 @@ package com.example.traffy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import java.lang.reflect.Array;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button runButton;
-    private TextView readme;
-
+    private TextView trafic;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        runButton = (Button) findViewById(R.id.runButton);
-        p
-        readme = (TextView) findViewById(R.id.readme);
+        trafic = findViewById(R.id.traffic);
+        runButton = findViewById(R.id.runButton);
 
+        runButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(trafic.getText().toString() == "Red"){
+                    trafic.setText("Yellow");
+                    trafic.setTextColor(getResources().getColor(R.color.Yellow));
+                }else if(trafic.getText().toString() == "Yellow"){
+
+                    trafic.setText("Green");
+                    trafic.setTextColor(getResources().getColor(R.color.Green));
+
+                }else{
+
+                    trafic.setText("Red");
+                    trafic.setTextColor(getResources().getColor(R.color.Red));
+                }
+            }
+        });
     }
+
 }
